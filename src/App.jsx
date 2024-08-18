190b90e5c273ceebf6935c007481ca88439e4cd3
William Espinoza <espinozaw657@gmail.com>
2024-08-18 11:24:29 -0500

    feat: issues fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { About } from "./components/about/about";
 import { Contact } from "./components/contact/contact";
 import { Footer } from "./components/footer/footer";
 import { Hero } from "./components/hero/hero";
-import { Navbar } from "./components/Navbar/navbar";
+import { Navbar } from "./components/navbar/navbar";
 import { Works } from "./components/works/works";
 
 function App() {
